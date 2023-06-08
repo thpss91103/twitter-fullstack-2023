deaf5c42eeb6e0987541f3634d2146368b1a3b81
Leechiayu <gary86442@gmail.com>
2023-06-08 20:00:10 +0800

    feat: add reply modal

--- models/reply.js
+++ models/reply.js
@@ -1,8 +1,30 @@
 'use strict'
-module.exports = (sequelize, DataTypes) => {
-  const Reply = sequelize.define('Reply', {
-  }, {})
-  Reply.associate = function (models) {
+const { DataTypes, Model } = require('sequelize')
+
+module.exports = sequelize => {
+  class Reply extends Model {
+    static associate(models) {
+      //* 回覆屬於tweet和user
+      Reply.belongsTo(models.Tweet, { foreignKey: 'tweetId' })
+      Reply.belongsTo(models.User, { foreignKey: 'userId' })
+      //* 回覆
+      Reply.hasMany(models.Reply, { foreignKey: 'tweetId' })
+      //* 喜歡
+      Reply.hasMany(models.Like, { foreignKey: 'tweetId' })
+    }
   }
+  Reply.init(
+    {
+      user_id: DataTypes.INTEGER,
+      tweet_id: DataTypes.INTEGER,
+      comment: DataTypes.TEXT
+    },
+    {
+      sequelize,
+      modelName: 'Reply',
+      tableName: 'Replies',
+      underscored: true
+    }
+  )
   return Reply
 }
